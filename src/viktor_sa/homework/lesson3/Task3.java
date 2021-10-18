6846f5066dfe83cf486361dee887a2b901cf594a
VIktarSazhin <sa.vitek98@gmail.com>
2021-10-18 21:49:35 +0300

    Refresh Task3

--- src/viktor_sa/homework/lesson3/Task3.java
+++ src/viktor_sa/homework/lesson3/Task3.java
@@ -2,15 +2,15 @@ package viktor_sa.homework.lesson3;
 
 /* Создать 2 пароля: основной и запасной.
 
-        * Ввести с клавиатуры 4 значный пароль (из цифр)
-        * Если пароль верный, написать "Hello"
-        * Если ввели верный запасной пароль, написать "Hello, change your password"
-        * Если неверный, написать "Wrong Password"
-        * Если пароль не из 4 цифр, написать ("Error");
+ * Ввести с клавиатуры 4 значный пароль (из цифр)
+ * Если пароль верный, написать "Hello"
+ * Если ввели верный запасной пароль, написать "Hello, change your password"
+ * Если неверный, написать "Wrong Password"
+ * Если пароль не из 4 цифр, написать ("Error");
 
-        */
+ */
 
-        import java.util.Scanner;
+import java.util.Scanner;
 
 public class Task3 {
 
@@ -23,33 +23,33 @@ public class Task3 {
         System.out.print("Please, enter your password: ");
         String enterPassword = scan.nextLine();
 
-        /**   switch (enterPassword) {
-            case "1111":
+        if (enterPassword.length() != 4) {
+            System.out.println("Error");
+        }
+
+        switch (enterPassword) {
+            case "1111": //mainPassword
                 System.out.println("Hello");
                 break;
 
-            case  "2222":
+            case "2222": //reservPassword
                 System.out.println("Hello, change your password");
-                break;
-
-            case incorrectPassword:
-                System.out.println("Error");
                 break;
 
             default:
                 System.out.println("Wrong Password");
         }
-*/
-              if (enterPassword.equals(mainPassword)) {
-         System.out.println("Hello");
-         } else if (enterPassword.equals(reservPassword)) {
-         System.out.println("Hello, change your password");
-         } else if (!enterPassword.equals(mainPassword) || enterPassword != reservPassword) {
-         System.out.println("Wrong Password");
-         } else {
-         System.out.println("Error");
-         }
 
+/**        if (enterPassword.equals(mainPassword)) {
+            System.out.println("Hello");
+        } else if (enterPassword.equals(reservPassword)) {
+            System.out.println("Hello, change your password");
+        } else if (enterPassword.length() != 4) {
+            System.out.println("Error");
+        } else {
+            System.out.println("Wrong password");
+        }
+*/
         scan.close();
     }
 }
