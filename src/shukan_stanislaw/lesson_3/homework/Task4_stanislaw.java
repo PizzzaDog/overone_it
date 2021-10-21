dcf6dacc1917b851c112397c06d599ed07d61998
Stanislaw <shukans588@gmail.com>
2021-10-21 22:56:33 +0300

    Домашка: таблица умножения

---
+++ src/shukan_stanislaw/lesson_3/homework/Task4_stanislaw.java
@@ -0,0 +1,14 @@
+package shukan_stanislaw.lesson_3.homework;
+
+import java.io.OutputStream;
+
+public class Task4_stanislaw {
+    public static void main(String[] args) {
+        for (int i = 1; i <= 10; i++) {
+            for (int j = 1; j <= 10; j++) {
+                System.out.print(i * j + " ");
+            }
+            System.out.println();
+        }
+    }
+}
