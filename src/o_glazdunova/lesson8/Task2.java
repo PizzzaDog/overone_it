016600fe24ccee989ce84a67b85f005b77484151
User <glazdunovao@mail.ru>
2021-10-27 23:04:51 +0300

    Test 1,2

---
+++ src/o_glazdunova/lesson8/Task2.java
@@ -0,0 +1,11 @@
+package o_glazdunova.lesson8;
+
+public class Task2 {
+    public static void main(String[] args) {
+        int nam = 2;
+        for (int i = 1; i <= 5; i++) {
+            nam = nam * nam - 1;
+            System.out.println(nam);
+        }
+    }
+}
