cf6e33334e2af89e50e36a6a2ad2a2a5ef7408e3
PizzzaDog <7883494@gmail.com>
2021-10-29 22:16:04 +0300

    Нерабочая версия

---
+++ src/n_bogdan/lesson8/answer/GameCase.java
@@ -0,0 +1,9 @@
+package n_bogdan.lesson8.answer;
+
+public class GameCase {
+    int wallet = 0; // Создаем кошелек
+    int bet = 0; // переменная для ставок
+    int choice = 0; // Переменная для 0 или 1
+    int result = 0;
+
+}
