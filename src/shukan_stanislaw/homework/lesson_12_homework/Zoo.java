9b1ce73bc4cce9fb6f4e87a317208c817e4849c0
Stanislaw <shukans588@gmail.com>
2021-11-12 17:58:15 +0300

    Zoo

---
+++ src/shukan_stanislaw/homework/lesson_12_homework/Zoo.java
@@ -0,0 +1,22 @@
+package shukan_stanislaw.homework.lesson_12_homework;
+
+import shukan_stanislaw.homework.lesson_12_homework.animals.Cat;
+import shukan_stanislaw.homework.lesson_12_homework.animals.Dog;
+import shukan_stanislaw.homework.lesson_12_homework.animals.Elephant;
+import shukan_stanislaw.homework.lesson_12_homework.service.AnimalService;
+
+public class Zoo {
+
+
+    public static void main(String[] args) {
+        AnimalService service = new AnimalService();
+        Cat cat = new Cat("Ryzhik", 4, "white");
+        Dog dog = new Dog("Jere", 3, "brown");
+        Elephant el = new Elephant("Jack", 8);
+        service.scream(cat);
+        service.scream(dog);
+        service.scream(el);
+        
+
+    }
+}
