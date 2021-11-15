1480da0c7c0beac63cb857434ccffbb1c0d7d5db
Stanislaw <shukans588@gmail.com>
2021-11-15 17:56:02 +0300

    zoo commit

---
+++ src/shukan_stanislaw/homework/lesson_12_homework/animals/Dog.java
@@ -0,0 +1,37 @@
+package shukan_stanislaw.homework.lesson_12_homework.animals;
+
+public class Dog {
+    private String name;
+    private int age;
+    private String eyesColor;
+
+    public Dog(String name, int age, String eyesColor) {
+        this.name = name;
+        this.age = age;
+        this.eyesColor = eyesColor;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getEyesColor() {
+        return eyesColor;
+    }
+
+    public void setEyesColor(String eyesColor) {
+        this.eyesColor = eyesColor;
+    }
+}
--- src/shukan_stanislaw/lesson_12/Main.java
+++ src/shukan_stanislaw/lesson_12/Main.java
@@ -4,20 +4,30 @@ import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) {
-        ArrayList<Integer> array = new ArrayList<>();//коллекция
+//        ArrayList<Integer> array = new ArrayList<>(); // {5, 10, 7, 2}
+//
+//        //Добавляем элемент в ArrayList
+//        array.add(5); // {5}
+//        array.add(7); // {5, 7}
+//        array.add(1, 10); // {5, 10, 7}
+//        array.add(2); // {5, 10, 7, 2}
+//
+//
+//        //Достать элемент по индексу не удаляя его
+//        array.get(1);
+//
+//        //Удаление элемента по индексу
+//        array.remove(1);
+//
+//        //Количество заполненных ячеек листа
+//        System.out.println(array.size());
+//
+//        for (int i : array) {
+//            System.out.println(i);
+//        }
 
-        array.add(5);
-        array.add(7);
-        array.add(1,10);
-        array.add(2);
 
-        System.out.println("array.remove(): " + array.remove(1));//удаляет 10ку и возвращает ее
-        System.out.println("Размер: " + array.size());
-//        array.contains();
 
-        for(int i: array){
-            System.out.println(i);
-        }
 
         ArrayList<Dog> dogList = new ArrayList<>();
         Dog dog1 = new Dog("Molly", 5);
@@ -27,54 +37,60 @@ public class Main {
         dogList.add(dog2);
         dogList.add(dog3);
 
-
-        System.out.println("does it contains dog2: " + (dogList.contains(dog2)));
-        for(Dog dog: dogList){
-            System.out.println(dog.getName() + " " + dog.getAge());
-        }
-//реализация через stream
-        System.out.println();
-        dogList.stream().forEach(x -> System.out.println(x.getName() + " " + x.getAge() ));
+        //Проверить наличие объекта в листе @return boolean
+        System.out.println(dogList.contains(dog2));
+        dogList.stream().forEach(x -> System.out.print(x.getName() + " "));
         System.out.println();
 
-        //dogList.remove(dog2);
-        dogList.remove(1);
+
+        //Достаем по ссылке на объект
         System.out.println(dogList.remove(dog2));
-        dogList.stream().forEach(x -> System.out.println(x.getName() + " " + x.getAge()));
-
+        dogList.stream().forEach(x -> System.out.print(x.getName() + " "));
         System.out.println();
 
-        //System.out.println(dogList.remove(dog2));
+        System.out.println(dogList.remove(dog2));
 
         dogList.add(dog2);
         dogList.add(dog2);
+        dogList.stream().forEach(x -> System.out.print(x.getName() + " "));
+        System.out.println();
+
+        /**
+         * ArrayList может хранить неограниченное кол-во одинаковых элементов (ссылок на них)
+         * Удаляем опо ссылке.
+         * @return boolean
+         */
         System.out.println(dogList.remove(dog2));
-        dog2.setName("dog2 nameRemastered");
+        dog2.setName("Dog 2 remastered");
         System.out.println(dogList.remove(dog2));
+        // dog2 закончились
         System.out.println(dogList.remove(dog2));
 
+        // Проверяет наличие элемента без его удаления @return boolean
+        dogList.contains(dog2);
+
+        // Индекс объекта
         dogList.indexOf(dog2);
+
+        // Замена объекта по индексу
         dogList.set(1, dog2);
 
-        for(Dog dog: dogList){
-            System.out.println(dog.getName() + " " + dog.getAge());
-        }
-
-        //dogList.get(10);
+        //
+//        dogList.get(10);
         System.out.println(dogList.get(dogList.size() - 1).getName());
-//
+
+        // Позволяет узнать, пустой ли лист @return boolean
+        dogList.isEmpty();
+
+        // NullPointerException наш лист не инициализирован
 //        ArrayList<Dog> arrayNull;
 //        arrayNull.isEmpty();
-//
-        System.out.println();
+
         ArrayList<Dog> dogsToAdd = new ArrayList<>();
         dogsToAdd.add(new Dog("qwe", 1));
-        dogsToAdd.add(new Dog("aqwer", 2));
+        dogsToAdd.add(new Dog("qwerr", 2));
         dogList.addAll(dogsToAdd);
 
-        for(Dog dog: dogList){
-            System.out.println(dog.getName() + " " + dog.getAge());
-        }
-    }
 
+    }
 }
