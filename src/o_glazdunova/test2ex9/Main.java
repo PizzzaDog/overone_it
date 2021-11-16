fa3a71648bdb562c75cc105f1e03e1a00ec35b25
User <glazdunovao@mail.ru>
2021-11-16 17:00:34 +0300

    ex 9 olga

---
+++ src/o_glazdunova/test2ex9/Main.java
@@ -0,0 +1,16 @@
+package o_glazdunova.test2ex9;
+
+public class Main {
+    public static void main(String[] args) {
+        Cake cake = new Cake();
+        Candy candy = new Candy();
+        Marshmallow marshmallow = new Marshmallow();
+        cake.bake();
+        cake.pleasure();
+        candy.flood();
+        candy. health();
+        marshmallow.dry();
+        marshmallow.taste();
+     }
+}
+// Написать 1 пример класса и его наследника. (+ вызов метода(ов) в main)
---
+++ src/o_glazdunova/test2ex9/Sweets.java
@@ -0,0 +1,15 @@
+package o_glazdunova.test2ex9;
+
+public class Sweets {
+    public void pleasure() { //удовольствие
+        System.out.println("Brings pleasure");// приносит удовольствие
+    }
+
+    public void taste() {// вкус
+        System.out.println("Gives a taste"); //дает вкус
+    }
+
+    public void health() { //здоровье
+        System.out.println("Bad for health");// вредит здоровью
+    }
+}
