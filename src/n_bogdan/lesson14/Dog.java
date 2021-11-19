2533c0e6ed4b64e162fc7e2fcc82142d072e1067
PizzzaDog <7883494@gmail.com>
2021-11-19 22:24:41 +0300

    lesson 14

---
+++ src/n_bogdan/lesson14/Dog.java
@@ -0,0 +1,27 @@
+package n_bogdan.lesson14;
+
+class Dog {
+    private String name;
+    private Integer age;
+
+    public Dog(String name, Integer age) {
+        this.name = name;
+        this.age = age;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+}
